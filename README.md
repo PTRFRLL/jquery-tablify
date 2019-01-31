6742289810cebfc48a71d348623f3ab6681053a7
Peter Fiorella <PTRFRLL@gmail.com>
2019-01-31 15:22:41 -0700

    Repo init

---
+++ README.md
@@ -0,0 +1,51 @@
+# jQuery Tablify
+
+Simple jQuery plugin that fills contents of an HTML table with JSON or JS array data. 
+
+## Usage
+
+Include tablify.js (and jQuery) in your project 
+```html
+<script src='jquery.js'></script>
+<script src='tablify.js'></script>
+```
+
+Intialize tablify on an empty table:
+
+```js
+$('#users').tablify(options);
+```
+
+### Options
+ - `data`: JSON or JS array (required)
+ - `capitalize`: will capitalize header items (default true)
+ - `uppercase`: will uppercase header names (default false) (overrides capitalize)
+
+
+## Examples
+```html
+
+<table id='users'>
+</table>
+
+<script> 
+    $(document).ready(function(){
+        let users = [
+            {name: 'Phillip J. Fry', occupation: 'Delivery Boy'}, 
+            {name: 'Bender Bending Rodriguez', occupation: 'Girder-bender'}, 
+            {name: 'Turanga Leela', occupation: 'Spaceship Captain'}
+        ];
+        
+        $('#users').tablify({ data: users});
+    });
+</script>
+```
+
+The above example will produce:
+| Name                     | Occupation        |
+|--------------------------|-------------------|
+| Phillip J. Fry           | Delivery Boy      |
+| Bender Bending Rodriguez | Girder-bender     |
+| Turanga Leela            | Spaceship Captain |
+
+
---
+++ tablify.js
@@ -0,0 +1,68 @@
+/**
+ * jquery-tablify
+ * Peter Fiorella
+ * 2019
+ */
+(function( $ ) {
+    
+    function cap(str){
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    function normalize(data){
+        try{
+            return JSON.parse(data);
+        }catch(e){
+            return data;
+        }
+    }
+
+    function maxKeys(data){
+        var max = 0;
+        for(var i = 0; i < data.length; i++){
+            var keyCount = Object.keys(data[i]).length;
+            if(keyCount > max){
+                max = keyCount;
+                index = i;
+            }
+        }
+        console.log(`Index with most keys: ${index} with ${max}`)
+        return index;
+    }
+
+    $.fn.tablify = function(options) {
+        let data = normalize(options.data);
+
+        let settings = $.extend({
+            capitalize: true,
+            uppercase: false
+        }, options);
+
+        if(!data || data.length == 0){
+            return this;
+        }
+        var mostKeys = maxKeys(data);
+        var keys = Object.keys(data[mostKeys]);
+        var header = keys.map(item => {
+            if(settings.capitalize){
+                item = cap(item);
+            }
+            if(settings.uppercase){
+                item = item.toUpperCase();
+            }
+            return `<th>${item}</th>`;
+        }).join('');
+        let thead = `<thead><tr>${header}</tr></thead>`;
+
+        let body = data.map(item => {
+            return "<tr>" + Object.keys(item).map(prop => {
+                return `<td>${item[prop]}</td>`
+            }).join('') + "</tr>";
+        }).join('');
+        let tbody = `<tbody>${body}</tbody>`;
+        let html = thead + tbody;
+        this.html(html);
+        return this;
+    };
+ 
+}( jQuery ));
